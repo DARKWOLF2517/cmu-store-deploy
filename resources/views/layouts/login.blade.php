71d8ebf0073ac13dbda37b096efcd947e084c55d
DARKWOLF2517 <jerrichopamisa@gmail.com>
2023-11-07 16:20:12 +0800

    asdf

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -14,7 +14,7 @@
   <link rel="icon" href="{{ url('https://i.pinimg.com/736x/00/eb/c7/00ebc7ed5034e9f18acae56a19966099--flat-illustration-stock-illustrations.jpg') }}">
 
   @vite('resources/js/app.js')
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body>
