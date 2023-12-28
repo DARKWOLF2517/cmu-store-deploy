51ae3f24e3ae8099b154c495108c06816d8b7138
ueng-lin <queenrbahay@gmail.com>
2023-12-28 18:31:55 +0800

    aaa

--- resources/views/attendance_checker/student_attendance_records.blade.php
+++ resources/views/attendance_checker/student_attendance_records.blade.php
@@ -11,7 +11,7 @@
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="/login/org_dashboard">Dashboard</a></li>
-                    <li class="breadcrumb-item">Student Organization</li>
+                    <li class="breadcrumb-item">Attendance Checker</li>
                     <li class="breadcrumb-item active" aria-current="page">Attendance</li>
                     <li class="breadcrumb-item active" aria-current="page">Attendance Records</li>
                 </ol>
