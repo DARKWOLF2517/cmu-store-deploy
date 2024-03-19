c536e249869bbc9768f474973389503469235b20
ueng-lin <queenrbahay@gmail.com>
2024-03-19 14:34:21 +0800

    sd

--- resources/views/mail/attendanceEmail.blade.php
+++ resources/views/mail/attendanceEmail.blade.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>test</h1>
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Email</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        line-height: 1.6;
+        background-color: #f4f4f4;
+        padding: 20px;
+      }
+      .container {
+        max-width: 600px;
+        margin: auto;
+        background: #fff;
+        padding: 20px;
+        border-radius: 5px;
+      }
+      .header {
+        background-color: #60b192;
+        color: #fff;
+        padding: 10px;
+        text-align: center;
+        border-radius: 5px 5px 0 0;
+      }
+      .content {
+        padding: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <h3>College of Information Technology</h3>
+      </div>
+      <div class="content">
+        <p>Hi User!</p>
+        <p>
+          You have logged into session 1 of the attendance for the General
+          Assembly.
+        </p>
+        <small>Date Attended: March 19, 2024</small>
+        <br>
+        <small>Time Attended: 10:00 AM</small>
+      </div>
+    </div>
+  </body>
 </html>
