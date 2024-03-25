ed5b76d2b442b6ecdff89165c1a6e50f563519d2
DARKWOLF2517 <jerrichopamisa@gmail.com>
2024-03-25 22:58:29 +0800

    asfd

--- resources/js/components/StudentOrg/Dashboard/DashboardAnnouncement.vue
+++ resources/js/components/StudentOrg/Dashboard/DashboardAnnouncement.vue
@@ -13,13 +13,25 @@
                 <p class="empty-schedule">Announcements show up here</p>
             </div>
             <div class="announcement-card-list">
-                    <button class="announcement-card btn btn-light text-start" style="width: 100%;" v-for="announcement in announcement" @click="openModal(announcement)">
-                        <span class="Organization text-success"><b>{{ announcement.title }}</b></span>
-                        <div class="date-time-posted">
-                            <span class="date-time-uploaded"><i><small>{{ announcement.date }}</small></i></span>
-                        </div>
-                    </button>
-                </div>
+
+                <button class="announcement-card btn btn-light text-start" style="width: 100%;"
+                    v-for="announcement in announcement" @click="openModal(announcement)">
+                    <div>
+                        <p class="d-flex align-items-center text-dark text-decoration-none " aria-expanded="false">
+                            <img v-if="announcement.organization.image" :src="announcement.organization.image"
+                                alt="user-image" width="32" height="32" class="rounded-circle me-2">
+                            <img v-else src="https://indonesiasatu.co.id/assets/themes/indonesiasatu/img/user.png"
+                                alt="user-image" width="32" height="32" class="rounded-circle me-2">
+                            <span class="profile-name"><strong>{{ this.selectedAnnouncement.organization
+                                    }}</strong></span>
+                        </p>
+                    </div>
+                    <span class="Organization text-success"><b>{{ announcement.title }}</b></span>
+                    <div class="date-time-posted">
+                        <span class="date-time-uploaded"><i><small>{{ announcement.date }}</small></i></span>
+                    </div>
+                </button>
+            </div>
         </div>
         <!-- Bootstrap 5 Modal -->
         <div class="modal" id="announcementModal" tabindex="-1">
@@ -31,17 +43,10 @@
                     </div>
                     <div class="modal-body">
                         <!-- Content here -->
-                        <div>
-                                <p class="d-flex align-items-center text-dark text-decoration-none "
-                                    aria-expanded="false">
-                                    <img
-                                        src="https://indonesiasatu.co.id/assets/themes/indonesiasatu/img/user.png"
-                                        alt="user-image" width="32" height="32" class="rounded-circle me-2">
-                                    <span class="profile-name"><strong>CSCo</strong></span>
-                                </p>
-                            </div>
-                        <small class="text-mute">Date and time posted: </small>
-                        <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos est praesentium itaque neque tenetur. Ab alias eaque blanditiis amet at similique quaerat labore laudantium, illum nemo totam debitis, quos eveniet.</p>
+
+                        <small class="text-mute">Date and time posted: {{ this.selectedAnnouncement.date }} {{
+        this.selectedAnnouncement.time }} </small>
+                        <p class="mt-2">{{ this.selectedAnnouncement.description }}</p>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -54,13 +59,21 @@
 
 <script>
 import { convertDate } from "../Functions/DateConverter.js";
+import { converTime } from "../Functions/TimeConverter.js";
 export default {
     props: ['org_id', 'school_year_session'],
     data() {
         return {
             announcement: [],
             loading: true,
-            selectedAnnouncement: {}
+            selectedAnnouncement: {
+                title: '',
+                organization: '',
+                date: '',
+                time: '',
+                description: '',
+                image: '',
+            }
         }
     },
     mounted() {
@@ -71,9 +84,9 @@ export default {
             axios.get(`/get_announcement/${this.org_id}/${this.school_year_session}`)
                 .then(response => {
                     const data = response.data;
-            data.forEach(item => {
-                item["date"] = convertDate(item["date"]);
-            });
+                    data.forEach(item => {
+                        item["date"] = convertDate(item["date"]);
+                    });
                     this.announcement = response.data
                     console.log(response.data)
                     this.loading = false;
@@ -83,7 +96,13 @@ export default {
                 });
         },
         openModal(announcement) {
-            this.selectedAnnouncement = announcement;
+            this.selectedAnnouncement.title = announcement.title;
+            this.selectedAnnouncement.organization = announcement.organization.name;
+            this.selectedAnnouncement.date = announcement.date;
+
+            this.selectedAnnouncement.time = converTime(announcement.time);
+            this.selectedAnnouncement.description = announcement.description;
+            this.selectedAnnouncement.image = announcement.organization.image;
             var modal = new bootstrap.Modal(document.getElementById('announcementModal'));
             modal.show();
         },
